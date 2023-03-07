fa3a0fe01d515631492e50bfbd1cdb00e52ca73b
livi <scholtz.ice@gmail.com>
2023-03-07 13:14:04 +0200

    Changed About section content

--- src/About.js
+++ src/About.js
@@ -7,16 +7,15 @@ function About() {
                 <h2 className="glass-card__heading">A Bit About Me</h2>
                 <p className="glass-card__text">
                     At present, I mostly work with Javascript and use Vue.js as
-                    my framework of choice. My development involves creating
-                    UI's that display data in a way that's pleasing and relevant
-                    to the end user. I would love to collaborate, read code that
-                    isn't my own, and accumulate invaluable knowledge working
-                    under and alongside other developers in my team. I take
-                    pride in my work and since the name of the game is lifelong
-                    learning, I'm sure to do just that in order to deliver
-                    products that exceed end user expectations. I'm currently
-                    learning Node.js and state management with Redux and see
-                    myself growing into a full-stack web based developer.
+                    my framework of choice. I would love to collaborate, read
+                    code that isn't my own, and accumulate invaluable knowledge
+                    working under and alongside other developers in a team. I
+                    take pride in my work and since the name of the game is
+                    lifelong learning, I'm sure to do just that in order to
+                    deliver products that exceed end user expectations. I'm
+                    currently learning Node.js and state management with Redux
+                    and see myself growing into a full-stack web based
+                    developer.
                 </p>
             </div>
         </div>
