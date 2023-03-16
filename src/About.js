41b473a3ddb0914df18a151def806e48c5a8de60
livi <scholtz.ice@gmail.com>
2023-03-16 15:20:32 +0200

    Changed about section card height

--- src/About.js
+++ src/About.js
@@ -6,15 +6,14 @@ function About() {
             <div className="about-glass-card">
                 <h2 className="about-glass-card__heading">A Bit About Me</h2>
                 <p className="about-glass-card__text">
-                    At present, I mostly work with Javascript and use Vue.js as
-                    my framework of choice. I would love to collaborate, read
-                    code that isn't my own, and accumulate invaluable knowledge
-                    working alongside other developers in a team. I take pride
-                    in my work and since the name of the game is lifelong
-                    learning, I'm sure to do just that in order to deliver
-                    products that exceed end user expectations. I'm currently
-                    learning Node.js and Redux and see myself growing into a
-                    full-stack web based developer.
+                    At present, I have experience working with Javascript and
+                    Vue.js. I would love to collaborate, read code that isn't my
+                    own, and accumulate invaluable knowledge working alongside
+                    other developers in a team. I take pride in my work and
+                    since the name of the game is lifelong learning, I'm sure to
+                    do just that in order to deliver products that exceed end
+                    user expectations. I'm currently learning Redux and see
+                    myself growing into a full-stack developer.
                 </p>
             </div>
         </div>
