f4ba3ad0efc7ab83f380929292c68de6e1c93480
livi <scholtz.ice@gmail.com>
2023-03-16 14:59:05 +0200

    Added fade-in effect to project cards

--- src/components/Route.js
+++ src/components/Route.js
@@ -3,8 +3,20 @@ import NavigationContext from "../context/navigation";
 
 function Route({ path, children }) {
     const { currentPath } = useContext(NavigationContext);
+    // const navigationPanel = document.querySelector(".navigation");
 
     if (path === currentPath) {
+        // console.log(navigationPanel.childNodes[3]);
+        // if (path === "/") {
+        //     console.log("yes");
+        // } else if (path === "/about") {
+        //     console.log("yes");
+        // } else if (path === "/projects") {
+        //     console.log("yes");
+        // } else if (path === "/contact") {
+        //     console.log("yes");
+        // }
+        // console.log(path);
         return children;
     }
 
