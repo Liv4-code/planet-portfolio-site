10d93b09463b621081b0d4471c55f2cf01812684
livi <scholtz.ice@gmail.com>
2023-04-10 13:05:07 +0200

    Decreased font size of home page heading

--- src/Home.js
+++ src/Home.js
@@ -10,8 +10,8 @@ function Home() {
                         typewriter
                             .pauseFor(1000)
                             .changeDelay(50)
-                            .typeString("<h2>Welcome, distant traveller</h2>")
-                            .typeString("<h1>My name is Olivia</h1>")
+                            .typeString("<h3>Welcome, distant traveller</h3>")
+                            .typeString("<h2>My name is Olivia</h2>")
                             .typeString("<h1>I'm a web developer</h1>")
                             .typeString("<h3>Come...</h3>")
                             .pauseFor(500)
