c1661c4be297d1c0a626600cb4f09a20902bcec6
livi <scholtz.ice@gmail.com>
2023-04-13 16:48:42 +0200

    Removed hover effect from nav buttons

--- src/Projects.js
+++ src/Projects.js
@@ -1,18 +1,18 @@
 import "./Projects.scss";
 import backgroundImg from "./assets/forest.webp";
-import { LazyLoadImage } from "react-lazy-load-image-component";
+// import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function Projects() {
     return (
         <div className="projects-container">
-            <LazyLoadImage
+            {/* <LazyLoadImage
                 src={backgroundImg}
                 width={1500}
                 height={800}
                 alt="Lush Jungle Background"
                 effect="opacity"
                 placeholderSrc={backgroundImg}
-            />
+            /> */}
             <div className="projects">
                 <div className="card">
                     <a
