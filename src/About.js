a09fa5ef3b3789f2ca96093ed69897fbb3bdc3e0
livi <scholtz.ice@gmail.com>
2023-04-13 13:37:39 +0200

    Changed about section content

--- src/About.js
+++ src/About.js
@@ -7,12 +7,11 @@ function About() {
                 <h2 className="about-glass-card__heading">A Bit About Me</h2>
                 <p className="about-glass-card__text">
                     At present, I have experience working with Javascript and
-                    Vue.js and React. I would love to collaborate, read code
-                    that isn't my own, and accumulate invaluable knowledge
-                    working alongside other developers in a team. I take pride
+                    React. I would love to collaborate and accumulate invaluable
+                    knowledge working alongside other developers. I take pride
                     in my work and since the name of the game is lifelong
                     learning, I'm sure to do just that in order to deliver
-                    products that exceed end user expectations. I'm currently
+                    projects that exceed end user expectations. I'm currently
                     learning Redux as well as Node.js, and see myself growing
                     into a full-stack developer.
                 </p>
