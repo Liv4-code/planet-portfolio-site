6a9284487f0dbfd318c696f55b3fd7473f689a9a
livi <scholtz.ice@gmail.com>
2023-04-27 14:30:45 +0200

    Changed email address on contact section

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
                 <h2 className="contact-glass-card__heading">Contact Me</h2>
                 <p className="contact-glass-card__text">
                     To get in touch, please feel free to reach me over{" "}
-                    <a href="mailto:scholtz.ice@gmail.com">email</a>,{" "}
+                    <a href="mailto:olivia@ekowebsolutions.com">email</a>,{" "}
                     <a
                         href="https://www.linkedin.com/in/olivia-s-5a37b982/"
                         target="_blank"
