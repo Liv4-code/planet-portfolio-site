02f8d0bff0a6ac7732dbd862e902bc7c6ff0ea20
livi <scholtz.ice@gmail.com>
2024-02-02 15:37:08 +0200

    Added fade-in effect to landing page heading

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,14 @@
 import "./Home.scss";
 import { Typewriter } from "react-simple-typewriter";
+import { gsap } from "gsap";
+
+// setting global animations timeline
+const timeline = gsap.timeline({
+    defaults: { duration: 1.5, ease: "power1.out" },
+});
+
+timeline.fromTo(".heading", { y: "-2vh" }, { y: 0 });
+timeline.fromTo(".heading", { opacity: 0 }, { opacity: 1 }, "<");
 
 function Home() {
     return (
